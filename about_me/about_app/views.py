e360e51f8691088fadf09e86955827483ffa7ddc
Eugene_Pro <proskuryakov.e@list.ru>
2024-01-29 14:25:37 +0300

    created views functions

--- about_me/about_app/views.py
+++ about_me/about_app/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+import logging
+from django.http import HttpResponse
+from lorem_text import lorem
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+
+def log(func):
+    def wrapper(request, *args, **kwargs):
+        response = func(request, *args, **kwargs)
+        logger.info(f'The function {func.__name__} was returned: Посещена страница {request.path}')
+        return response
+
+    return wrapper
+
+
+@log
+def index(request):
+    paragraph_length = 3
+    text = lorem.paragraph(paragraph_length)
+    html = f"""<h1 align="center">Один из моих первых Django-проектов</h1>    
+    <p align="center">
+        <b>{text}</b>
+    </p>
+    <br>
+    <p>
+        {text * 2}
+    </p>
+    """
+    # logger.info(f'Посещена страница: {request.path}')
+    return HttpResponse(html)
+
+
+@log
+def about(request):
+    paragraph_length = 5
+    text = lorem.paragraph(paragraph_length)
+    html = f"""<h1 align="center">Обо мне</h1>    
+    <p align="center">
+        {text}
+    </p>    
+    """
+    # logger.info(f'Посещена страница: {request.path}')
+    return HttpResponse(html)
